0c7121e7207c7bbb099040755cbae33f2f43e024
rahul singh <95570957+rahul-singh2021@users.noreply.github.com>
2023-05-24 21:20:16 -0700

    Create transpose_of_a_matrix.py

---
+++ transpose_of_a_matrix.py
@@ -0,0 +1,15 @@
+#create a matrix to be transposed
+a=[[1,2,3],
+   [4,5,6],
+   [7,8,9]]
+#create a empty matrix to store the transpose of matrix a
+b=[[0,0,0].
+   [0,0,0],
+   [0,0,0]]
+for i in range(len(a)):
+  for j in range(len(a(0))):
+    b[i][j]=a[j][i]
+    
+#print the matrix b
+for x in b:
+  print(x)
