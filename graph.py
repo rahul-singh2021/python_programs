948accfd425603b8898e5515237e2e198ef62589
rahul singh <95570957+rahul-singh2021@users.noreply.github.com>
2023-05-26 22:45:34 -0700

    Create graph.py
    this a python program to make a graph

---
+++ graph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x_axis=[7,2,5,7,9,4]
+y_axis=[4,7,9,4,7,1]
+
+plt.plot(x,y)
+plt.xlabel("number of students")
+plt.ylabel("age of students")
+plt.title("graph to show age of students in a class")
+
+plt.show()
